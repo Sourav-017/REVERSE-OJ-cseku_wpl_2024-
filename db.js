6e47aa2020175287ca8d3b5ae69a7de4976234df
Sourav-017 <sourav2117@cseku.ac.bd>
2024-10-15 19:52:33 +0600

    Push

---
+++ db.js
@@ -0,0 +1,21 @@
+// db.js (MySQL connection setup)
+const mysql = require("mysql2");
+
+// Create a connection to the database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root", // Your MySQL username
+  password: "", // Your MySQL password
+  database: "oj", // Your database name
+});
+
+// Connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL: ", err);
+    return;
+  }
+  console.log("Connected to MySQL database!");
+});
+
+module.exports = connection;
